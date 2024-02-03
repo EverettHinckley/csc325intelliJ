8506bffa211c06f21263c3efc551fd5f4d262d1c
Everett Hinckley <hincea@farmingdale.edu>
2024-02-03 18:06:05 -0500

    Cloned Repo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello Everett Hinckley!");
+        System.out.println("Hello Everett Hinckley!");
 
 
     }
